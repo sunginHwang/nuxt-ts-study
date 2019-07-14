2d366d057b9082c2c419564a5542920421008cea
sunginHwang <gommpo111@gmail.com>
2019-07-14 22:53:27 +0900

    setting nuxt typeScript

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="title">
+    {{ name }}
+  </h1>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'nuxt-property-decorator'
+  import Logo from '~/components/Logo.vue'
+
+  @Component({
+    components: {
+      Logo
+    }
+  })
+  export default class IndexPage extends Vue {
+    name: string = 'hello-nuxt-ts-decorator'
+  }
+</script>
