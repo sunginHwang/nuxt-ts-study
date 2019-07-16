370deb35b1f9dcbbc5ccaf56b5167ed54c501bc4
hwangseong-in <gommpo111@gmail.com>
2019-07-16 19:02:32 +0900

    make class vue component

---
+++ components/base-fab-button.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="wrapper">
+        <div class="header">
+            testArea
+        </div>
+        <div class="content">
+            <p class="text"> {{content}}</p>
+        </div>
+        <div class="footer">
+            <button>버튼입니다.</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Vue} from 'nuxt-property-decorator';
+
+
+    export default class BaseFabButton extends Vue {
+        content: string = 'content'
+    }
+</script>
+
+<style>
+    .wrapper {
+        position: fixed;
+        display: flex;
+        flex-direction: column;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background-color: #fff;
+        padding: 0 30px;
+    }
+
+    .header {
+        margin: 30px 0;
+        height: 52px;
+        width: 100%;
+        background-color: #41b883;
+        color: #fff;
+    }
+
+    .content {
+        width: 100%;
+        border: 1px solid #41b883;
+        flex: auto;
+        margin-bottom: 30px;
+
+    }
+
+    .text {
+        width: 100%;
+        height: 100%;
+    }
+
+    .footer {
+        height: 52px;
+        width: 100%;
+        color: #fff;
+        margin-bottom: 30px;
+        background-color: orangered;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,21 @@
 <template>
-  <div class="wrapper">
-    <div class="header">
-      testArea
-    </div>
-    <div class="content">
-      <textarea class="text"/>
-    </div>
-    <div class="footer">
-      <button>버튼입니다.</button>
-    </div>
-  </div>
+ <div>
+   <base-fab-button :content="`this is content`"/>
+ </div>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'nuxt-property-decorator'
+  import {Component, Vue} from 'nuxt-property-decorator';
+  import BaseFabButton from '~/components/base-fab-button';
 
 
+  @Component({
+    components:{
+      BaseFabButton
+    }
+  })
   export default class IndexPage extends Vue {
     name: string = 'hello-nuxt-ts-decorator'
   }
 </script>
 
-<style>
-  .wrapper {
-    position: fixed;
-    display: flex;
-    flex-direction: column;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background-color: #fff;
-    padding: 0 30px;
-  }
-
-  .header {
-    margin: 30px 0;
-    height: 52px;
-    width: 100%;
-    background-color: #41b883;
-    color: #fff;
-  }
-
-  .content {
-    width: 100%;
-    border: 1px solid #41b883;
-    flex: auto;
-    margin-bottom: 30px;
-
-  }
-
-  .text {
-    width: 100%;
-    height: 100%;
-  }
-
-  .footer {
-    height: 52px;
-    width: 100%;
-    color: #fff;
-    margin-bottom: 30px;
-    background-color: orangered;
-  }
-</style>
